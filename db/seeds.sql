b5b404c880f5e36f5ce853586cb650a00891e4c1
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-08-24 09:22:42 -0700

    set up connection and orm

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Jalopeno Cheddar Burger', false),
+('Alfalfa Mushroom Veggie Burger', false),
+('Chicken Avacado Swiss Burger', false);
