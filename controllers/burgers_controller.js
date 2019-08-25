8e009b13bc85b50509d4995ccc1b6eaddad9cae3
kevinroymcgregor <kevin.mcgregor85204@gmail.com>
2019-08-24 22:18:40 -0700

    created files and verified working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    burger.insertOne(["name", "devoured"], [req.body.name, false], function (result) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.name, false], function (result) {
         res.json({ id: result.insertId });
     });
 });
